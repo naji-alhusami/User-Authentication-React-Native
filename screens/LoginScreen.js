031500a4734099cdfa19df15e390aa52be2f52f8
naji-alhusami <najihussami@gmail.com>
2024-02-24 00:37:32 +0100

    added context to handle login and logout

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,28 @@
-import AuthContent from '../components/Auth/AuthContent';
+import { useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { login } from "../util/auth";
+import { Alert } from "react-native";
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function loginHandler({ email, password }) {
+    setIsAuthenticating(true);
+
+    try {
+      await login(email, password);
+    } catch (error) {
+      Alert.alert("Authenticaiton Failed", "Could Not Log you in");
+    }
+    setIsAuthenticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Creating User ..." />;
+  }
+
+  return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
 
 export default LoginScreen;
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,28 @@
-import AuthContent from '../components/Auth/AuthContent';
+import { useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import { createUser } from "../util/auth";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
 
 function SignupScreen() {
-  return <AuthContent />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function signupHandler({ email, password }) {
+    setIsAuthenticating(true);
+
+    try {
+      await createUser(email, password);
+    } catch (error) {
+      Alert.alert("Authenticaiton Failed", "Could Not Create User");
+    }
+    
+    setIsAuthenticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Creating User ..." />;
+  }
+
+  return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
